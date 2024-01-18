77c12a0548af73cf84be5ab5d45975b6f169df3c
Nils Nahooy <nils.nahooy@live.com>
2024-01-18 19:24:04 +0100

    add interaction to pauze screen

--- src/pauze_screen/mod.rs
+++ src/pauze_screen/mod.rs
@@ -4,7 +4,7 @@ use crate::game::SimulationState;
 
 use systems::layout::spawn_pauze_screen;
 
-use self::systems::layout::despawn_pauze_screen;
+use self::systems::{interactions::interact_with_continue_button, layout::despawn_pauze_screen};
 
 mod components;
 mod styles;
@@ -15,6 +15,7 @@ pub struct PauzeScreenPlugin;
 impl Plugin for PauzeScreenPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(SimulationState::Paused), spawn_pauze_screen)
+            .add_systems(Update, interact_with_continue_button)
             .add_systems(OnExit(SimulationState::Paused), despawn_pauze_screen);
     }
 }
---
+++ src/pauze_screen/systems/interactions.rs
@@ -0,0 +1,20 @@
+use bevy::prelude::*;
+
+use crate::{pauze_screen::components::ContinueButton, game::SimulationState, shared::styles::{PRESSED_BUTTON_COLOR, HOVERED_BUTTON_COLOR, NORMAL_BUTTON_COLOR}};
+
+pub fn interact_with_continue_button(
+    mut button_query: Query<(&Interaction, &mut BackgroundColor),
+    (Changed<Interaction>, With<ContinueButton>)>,
+    mut simulation_state_next_state: ResMut<NextState<SimulationState>>
+){
+    if let Ok((interaction, mut background_color)) = button_query.get_single_mut(){
+        match *interaction{
+            Interaction::Pressed => {
+                *background_color = PRESSED_BUTTON_COLOR.into();
+                simulation_state_next_state.set(SimulationState::Running);
+            }
+            Interaction::Hovered => *background_color = HOVERED_BUTTON_COLOR.into(),
+            _ => *background_color = NORMAL_BUTTON_COLOR.into(),
+        }
+    }
+}
