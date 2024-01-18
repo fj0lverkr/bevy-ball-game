099c29d58edcfa11b63c946d7511c261ef8fc7b7
Nils Nahooy <nils.nahooy@live.com>
2024-01-18 20:57:50 +0100

    clean up code

--- src/pauze_screen/systems/interactions.rs
+++ src/pauze_screen/systems/interactions.rs
@@ -1,14 +1,20 @@
 use bevy::prelude::*;
 
-use crate::{pauze_screen::components::ContinueButton, game::SimulationState, shared::styles::{PRESSED_BUTTON_COLOR, HOVERED_BUTTON_COLOR, NORMAL_BUTTON_COLOR}};
+use crate::{
+    game::SimulationState,
+    pauze_screen::components::ContinueButton,
+    shared::styles::{HOVERED_BUTTON_COLOR, NORMAL_BUTTON_COLOR, PRESSED_BUTTON_COLOR},
+};
 
 pub fn interact_with_continue_button(
-    mut button_query: Query<(&Interaction, &mut BackgroundColor),
-    (Changed<Interaction>, With<ContinueButton>)>,
-    mut simulation_state_next_state: ResMut<NextState<SimulationState>>
-){
-    if let Ok((interaction, mut background_color)) = button_query.get_single_mut(){
-        match *interaction{
+    mut button_query: Query<
+        (&Interaction, &mut BackgroundColor),
+        (Changed<Interaction>, With<ContinueButton>),
+    >,
+    mut simulation_state_next_state: ResMut<NextState<SimulationState>>,
+) {
+    if let Ok((interaction, mut background_color)) = button_query.get_single_mut() {
+        match *interaction {
             Interaction::Pressed => {
                 *background_color = PRESSED_BUTTON_COLOR.into();
                 simulation_state_next_state.set(SimulationState::Running);
